ec142f368b2aef2b0ea071e8d5070f688a478d21
lauraaraujoo <lauragabriely2@gmail.com>
2024-03-08 19:26:58 -0300

    first commit

---
+++ src/components/Comment.tsx
@@ -0,0 +1,51 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css'; 
+import { Avatar } from './Avatar';
+import { useState } from 'react';
+
+interface CommentProps {
+    content: string;
+    onDeleteComment: (comment: string) => void;
+}
+
+export function Comment({content, onDeleteComment}: CommentProps) {
+
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    }
+
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1
+        });
+    }
+
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false}src="https://github.com/lauraaraujoo.png" alt="" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Laura Araujo</strong>
+                            <time title="25 de Janeiro ás 19:10h" dateTime="2024-01-25 19:10:45">Cerca de 1h átras</time>
+                        </div>
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
+                            <Trash size={24} />
+                        </button>
+                    </header>
+                    <p>{content}</p>
+                </div>
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp />
+                        Aplaudir <span>{likeCount}</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    );
+}
